2fca82ea8702ed50414f363ce89cf0d600be8832
corykim0829 <corykim0829@gmail.com>
2020-05-04 17:19:21 +0900

    chore: 프로젝트 생성
    Issue: #4

---
+++ client-mobile/BaseballGameApp/BaseballGameApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseballGameApp
+//
+//  Created by Cory Kim on 2020/05/04.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
