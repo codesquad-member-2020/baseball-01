0c4dff15bd845949618dd5bc06f0e537c058addb
corykim0829 <corykim0829@gmail.com>
2020-05-04 20:11:58 +0900

    feat: AutoLayout extension 추가
    UIView를 확장하여 autoLayout을 쉽고 편하게 줄 수 있도록 구현
    
    Issue: #6

---
+++ client-mobile/BaseballGameApp/BaseballGameApp/Extensions/AutoLayout.swift
@@ -0,0 +1,40 @@
+//
+//  AutoLayout.swift
+//  BaseballGameApp
+//
+//  Created by Cory Kim on 2020/05/04.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func constraints(topAnchor: NSLayoutYAxisAnchor?, leadingAnchor: NSLayoutXAxisAnchor?, bottomAnchor: NSLayoutYAxisAnchor?, trailingAnchor: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let topAnchor = topAnchor {
+            self.topAnchor.constraint(equalTo: topAnchor, constant: padding.top).isActive = true
+        }
+        
+        if let leadingAnchor = leadingAnchor {
+            self.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding.left).isActive = true
+        }
+        
+        if let bottomAnchor = bottomAnchor {
+            self.bottomAnchor.constraint(equalTo: bottomAnchor, constant: padding.bottom).isActive = true
+        }
+        
+        if let trailingAnchor = trailingAnchor {
+            self.trailingAnchor.constraint(equalTo: trailingAnchor, constant: padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+    }
+}
