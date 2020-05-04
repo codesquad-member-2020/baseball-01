9a73be5052ada4957ca40194c19e78ae83da0ca9
corykim0829 <corykim0829@gmail.com>
2020-05-04 20:37:02 +0900

    feat: Labels 구현
    title, description Label을 구현하여 추가
    
    Issue: #6

--- client-mobile/BaseballGameApp/BaseballGameApp/GameListViewController.swift
+++ client-mobile/BaseballGameApp/BaseballGameApp/GameListViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class GameListViewController: UIViewController {
+    
+    private let titleLabel = PlainLabel(text: "Game List", color: .white, fontSize: 32, weight: .semibold, alignment: .center)
+    private let descriptionLabel = PlainLabel(text: "플레이할 게임을 선택하세요", color: .white, fontSize: 16, weight: .medium, alignment: .center)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
 
-
+    private func configureUI() {
+        view.backgroundColor = .black
+        
+        view.addSubview(titleLabel)
+        view.addSubview(descriptionLabel)
+        titleLabel.constraints(topAnchor: view.safeAreaLayoutGuide.topAnchor, leadingAnchor: view.leadingAnchor, bottomAnchor: nil, trailingAnchor: view.trailingAnchor, padding: .init(top: 80, left: 80, bottom: 0, right: -80))
+        descriptionLabel.constraints(topAnchor: titleLabel.bottomAnchor, leadingAnchor: view.leadingAnchor, bottomAnchor: nil, trailingAnchor: view.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0))
+    }
 }
 
