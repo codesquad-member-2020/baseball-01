4dab90a61cbff4f9191a02b3dbee3507ef817049
corykim0829 <corykim0829@gmail.com>
2020-05-04 22:36:33 +0900

    feat: GameListCollectionView 표시
    GameListViewController에서 추가 및 autoLayout 지정
    
    Issue: #6

--- client-mobile/BaseballGameApp/BaseballGameApp/GameListViewController.swift
+++ client-mobile/BaseballGameApp/BaseballGameApp/GameListViewController.swift
@@ -12,6 +12,7 @@ class GameListViewController: UIViewController {
     
     private let titleLabel = PlainLabel(text: "Game List", color: .white, fontSize: 32, weight: .semibold, alignment: .center)
     private let descriptionLabel = PlainLabel(text: "플레이할 게임을 선택하세요", color: .white, fontSize: 16, weight: .medium, alignment: .center)
+    private let collectionView = GameListCollectionView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,8 +25,9 @@ class GameListViewController: UIViewController {
         
         view.addSubview(titleLabel)
         view.addSubview(descriptionLabel)
+        view.addSubview(collectionView)
         titleLabel.constraints(topAnchor: view.safeAreaLayoutGuide.topAnchor, leadingAnchor: view.leadingAnchor, bottomAnchor: nil, trailingAnchor: view.trailingAnchor, padding: .init(top: 80, left: 80, bottom: 0, right: -80))
         descriptionLabel.constraints(topAnchor: titleLabel.bottomAnchor, leadingAnchor: view.leadingAnchor, bottomAnchor: nil, trailingAnchor: view.trailingAnchor, padding: .init(top: 8, left: 0, bottom: 0, right: 0))
+        collectionView.constraints(topAnchor: descriptionLabel.bottomAnchor, leadingAnchor: view.leadingAnchor, bottomAnchor: view.safeAreaLayoutGuide.bottomAnchor, trailingAnchor: view.trailingAnchor, padding: .init(top: 28, left: 32, bottom: -16, right: -32))
     }
 }
-
