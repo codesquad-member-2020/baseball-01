f32d261f0dc8be7fa16eaa16de5f6f1b8ea33e9a
corykim0829 <corykim0829@gmail.com>
2020-05-06 10:34:48 +0900

    feat: popupView 추가
    Issue: #6

---
+++ client-mobile/BaseballGameApp/BaseballGameApp/Views/GamePopupView.swift
@@ -0,0 +1,29 @@
+//
+//  GamePopupView.swift
+//  BaseballGameApp
+//
+//  Created by Cory Kim on 2020/05/04.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class GamePopupView: UIView {
+    
+    private let cornerRadius: CGFloat = 8.0
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        backgroundColor = .red
+        layer.cornerRadius = cornerRadius
+    }
+}
