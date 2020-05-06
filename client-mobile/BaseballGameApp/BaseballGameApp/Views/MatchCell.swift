77d071075ca9b66a0a85f7a93bac74397a5f25b7
corykim0829 <corykim0829@gmail.com>
2020-05-06 21:47:24 +0900

    feat: MatchCell 내부 UI 구현
    Programmatically하게 오토레이아웃을 이용하여 구현
    오토레이아웃을 잘 줬다고 생각했는데, 이미지를 넣게 되면 이미지뷰가 커지게 되어서 위, 아래에 있는 라벨들이 깨지는 현상 발생
    위 아래에 있는 라벨들의 vertical compressionResistance 값을 높여주어서 깨지지 않도록 구현
    
    Issue: #6
    Resolved: #12

--- client-mobile/BaseballGameApp/BaseballGameApp/Views/MatchCell.swift
+++ client-mobile/BaseballGameApp/BaseballGameApp/Views/MatchCell.swift
@@ -11,11 +11,19 @@ import UIKit
 class MatchCell: UICollectionViewCell {
     
     static let identifier = "MatchCell"
-    static let heightRatio: CGFloat = 0.32
+    static let heightRatio: CGFloat = 0.38
+    
+    private let awayLogoImageView = LogoImageView()
+    private let homeLogoImageView = LogoImageView()
+    private let awayLabel = PlainLabel(text: "AWAY", color: .red, fontSize: 13, weight: .medium, alignment: .center)
+    private let homeLabel = PlainLabel(text: "HOME", color: .red, fontSize: 13, weight: .medium, alignment: .center)
+    private let versusLabel = PlainLabel(text: "VS", color: .black, fontSize: 23, weight: .heavy, alignment: .center)
+    
+    private let awayNameLabel = PlainLabel(text: "AWAY", color: .black, fontSize: 17, weight: .bold, alignment: .center)
+    private let homeNameLabel = PlainLabel(text: "HOME", color: .black, fontSize: 17, weight: .bold, alignment: .center)
     
     private let containerView = UIView()
-    
-    private let cornerRadius: CGFloat = 12
+    private let cornerRadius: CGFloat = 24
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -32,5 +40,39 @@ class MatchCell: UICollectionViewCell {
         containerView.layer.cornerRadius = cornerRadius
         containerView.backgroundColor = .init(white: 1, alpha: 0.9)
         containerView.constraints(topAnchor: topAnchor, leadingAnchor: leadingAnchor, bottomAnchor: bottomAnchor, trailingAnchor: trailingAnchor)
+        containerView.addSubview(versusLabel)
+        versusLabel.centerInSuperView()
+        
+        // Home-Away Labels
+        let verticalSpacingFromTeamLabelToContainer: CGFloat = 12.0
+        
+        containerView.addSubview(awayLabel)
+        awayLabel.constraints(topAnchor: containerView.topAnchor, leadingAnchor: containerView.leadingAnchor, bottomAnchor: nil, trailingAnchor: containerView.centerXAnchor, padding: .init(top: verticalSpacingFromTeamLabelToContainer, left: 0, bottom: 0, right: 0))
+        awayLabel.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 900), for: .vertical)
+        containerView.addSubview(homeLabel)
+        homeLabel.constraints(topAnchor: containerView.topAnchor, leadingAnchor: containerView.centerXAnchor, bottomAnchor: nil, trailingAnchor: containerView.trailingAnchor, padding: .init(top: verticalSpacingFromTeamLabelToContainer, left: 0, bottom: 0, right: 0))
+        homeLabel.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 900), for: .vertical)
+        
+        // LogoImageViews
+        let verticalSpacingFromLogoImageToTeamLabel: CGFloat = 4.0
+        
+        containerView.addSubview(awayLogoImageView)
+        awayLogoImageView.centerXAnchor.constraint(equalTo: awayLabel.centerXAnchor).isActive = true
+        awayLogoImageView.constraints(topAnchor: awayLabel.bottomAnchor, leadingAnchor: nil, bottomAnchor: nil, trailingAnchor: nil, padding: .init(top: verticalSpacingFromLogoImageToTeamLabel, left: 0, bottom: 0, right: 0))
+        
+        containerView.addSubview(homeLogoImageView)
+        homeLogoImageView.centerXAnchor.constraint(equalTo: homeLabel.centerXAnchor).isActive = true
+        homeLogoImageView.constraints(topAnchor: homeLabel.bottomAnchor, leadingAnchor: nil, bottomAnchor: nil, trailingAnchor: nil, padding: .init(top: verticalSpacingFromLogoImageToTeamLabel, left: 0, bottom: 0, right: 0))
+        
+        // TeamNameLabels
+        let verticalSpacingFromNameLabelToLogoImage: CGFloat = 4.0
+        let verticalSpacingFromContainerToNameLabel: CGFloat = 12.0
+        
+        containerView.addSubview(awayNameLabel)
+        awayNameLabel.constraints(topAnchor: awayLogoImageView.bottomAnchor, leadingAnchor: containerView.leadingAnchor, bottomAnchor: containerView.bottomAnchor, trailingAnchor: containerView.centerXAnchor, padding: .init(top: verticalSpacingFromNameLabelToLogoImage, left: 0, bottom: -verticalSpacingFromContainerToNameLabel, right: 0))
+        awayNameLabel.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 900), for: .vertical)
+        containerView.addSubview(homeNameLabel)
+        homeNameLabel.constraints(topAnchor: homeLogoImageView.bottomAnchor, leadingAnchor: containerView.centerXAnchor, bottomAnchor: containerView.bottomAnchor, trailingAnchor: containerView.trailingAnchor, padding: .init(top: verticalSpacingFromNameLabelToLogoImage, left: 0, bottom: -verticalSpacingFromContainerToNameLabel, right: 0))
+        homeNameLabel.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 900), for: .vertical)
     }
 }
