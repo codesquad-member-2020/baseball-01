b33374a45b67a3f1fd26c257e2ab51f841c4f97f
baekCode <sosaer88@gmail.com>
2020-05-07 18:20:27 +0900

    feat: keyframe 구현 및 초기화면 구현
    issue #14

--- client-web/baseball/src/Components/Intro/index.js
+++ client-web/baseball/src/Components/Intro/index.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
 import { IntroBg } from '../Styles/Backgorund';
-import {StartLink} from '../Styles/Link';
+import { StartLink } from './StartLink';
+import { introTitleMove } from '../Styles/Keyframes';
 import Title from './Title';
+import { Loader } from './Loader';
 
 const IntroDiv = styled.div`
   ${IntroBg}
   color: #fff;
 `;
 
+const TitleDiv = styled.div`
+  text-align: center;
+  opacity: 0;
+  ${props =>
+    props.disappear &&
+    css`
+      animation-duration: 1.5s;
+      animation-timing-function: ease-out;
+      animation-name: ${introTitleMove};
+      animation-fill-mode: forwards;
+    `}
+`;
+
 const Intro = () => {
+  const [loader, setLoader] = useState(true);
+  const [visible, setVisible] = useState(false);
+  setTimeout(() => {
+    setLoader(false);
+    setVisible(true);
+  }, 2000);
   return (
     <IntroDiv>
-      <Title />
-      <StartLink to='/match'>GAME START</StartLink>
+      {loader && <Loader />}
+      <TitleDiv disappear={visible}>
+        <Title />
+      </TitleDiv>
+      <StartLink link='/match' name='GAME START' disappear={visible} />
     </IntroDiv>
   );
 };
