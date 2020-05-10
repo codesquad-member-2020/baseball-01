ccbd816b2b866b8bd731f133e907243aea729ecd
suhyunsim <cnc1106@naver.com>
2020-05-11 00:30:40 +0900

    feat: 매치 리스트의 TeamDto 구현
    Builder 패턴을 활용해서 매치 리스트를 위한 팀 별 정보를 담은 TeamDto를 구현한다.
    
    Issue #25

---
+++ be/src/main/java/com/codesquad/baseball1/dto/TeamDto.java
@@ -0,0 +1,48 @@
+package com.codesquad.baseball1.dto;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class TeamDto {
+
+    @JsonProperty("team_name")
+    private final String teamName;
+
+    @JsonProperty("logo_image")
+    private final String logoImage;
+
+    @JsonProperty("player")
+    private final String player;
+
+    public TeamDto(Builder builder) {
+        teamName = builder.teamName;
+        logoImage = builder.logoImage;
+        player = builder.player;
+    }
+
+    public static class Builder {
+
+        private String teamName;
+        private String logoImage;
+        private String player;
+
+        public Builder (String teamName) {
+            this.teamName = teamName;
+        }
+
+        public Builder logoIamge(String logoImage) {
+            this.logoImage = logoImage;
+            return this;
+        }
+
+        public Builder player(String player) {
+            this.player = player;
+            return this;
+        }
+
+        public TeamDto build() {
+            return new TeamDto(this);
+        }
+    }
+}
