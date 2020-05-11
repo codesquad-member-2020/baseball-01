fc6617d014df7eb56fd78e11b6b2a2089764c229
suhyunsim <cnc1106@naver.com>
2020-05-11 19:59:45 +0900

    refactor: ResponseDto @JsonProperty 추가

--- be/src/main/java/com/codesquad/baseball1/dto/ResponseDto.java
+++ be/src/main/java/com/codesquad/baseball1/dto/ResponseDto.java
@@ -2,14 +2,18 @@ package com.codesquad.baseball1.dto;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class ResponseDto {
 
+    @JsonProperty("status_code")
     private int statusCode;
 
+    @JsonProperty("message")
     private String message;
 
+    @JsonProperty("data")
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Object data;
 
