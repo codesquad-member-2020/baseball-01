8636085718831a1c86daab02c8a21ce80c0678a4
corykim0829 <corykim0829@gmail.com>
2020-05-12 23:08:14 +0900

    chore: board color 추가
    Issue: #22

---
+++ client-mobile/BaseballGameApp/BaseballGameApp/Views/Play/CountBoardView.swift
@@ -0,0 +1,26 @@
+//
+//  CountBoardView.swift
+//  BaseballGameApp
+//
+//  Created by Cory Kim on 2020/05/11.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class CountBoardView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        
+    }
+}
