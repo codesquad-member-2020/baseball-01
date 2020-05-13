172ed960a83799b22fbd56ce0972239766d25bfe
corykim0829 <corykim0829@gmail.com>
2020-05-13 20:17:56 +0900

    chore: CountBoardView에 사용될 Color Set 추가
    Issue: #22

---
+++ client-mobile/BaseballGameApp/BaseballGameApp/Views/Play/CountBoardView.swift
@@ -0,0 +1,21 @@
+//
+//  CountBoardView.swift
+//  BaseballGameApp
+//
+//  Created by Cory Kim on 2020/05/13.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class CountBoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
