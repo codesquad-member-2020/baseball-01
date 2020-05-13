2d40a8425233784653790ea359b54e2bff7012df
corykim0829 <corykim0829@gmail.com>
2020-05-13 22:43:15 +0900

    feat: CurrentPitcherHitterView 구현
    Xib를 사용한 구현
    
    Issue: #22

---
+++ client-mobile/BaseballGameApp/BaseballGameApp/Views/Play/CurrentPitcherHitterView.swift
@@ -0,0 +1,48 @@
+//
+//  CurrentPitcherHitterView.swift
+//  BaseballGameApp
+//
+//  Created by Cory Kim on 2020/05/13.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CurrentPitcherHitterView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var pitcherNameLabel: UILabel!
+    @IBOutlet weak var hitterNameLabel: UILabel!
+    @IBOutlet weak var pitcherTeamLogoImageView: UIImageView!
+    @IBOutlet weak var hitterTeamLogoImageView: UIImageView!
+    @IBOutlet weak var pitcherInfoLabel: UILabel!
+    @IBOutlet weak var hitterInfoLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        let bundle = Bundle(for: Self.self)
+        bundle.loadNibNamed(String(describing: Self.self), owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        configureUI()
+    }
+    
+    private func configureUI() {
+        contentView.alpha = 0.9
+        contentView.layer.cornerRadius = 8
+        contentView.layer.borderColor = UIColor.black.cgColor
+        contentView.layer.borderWidth = 1.5
+        contentView.clipsToBounds = true
+    }
+}
