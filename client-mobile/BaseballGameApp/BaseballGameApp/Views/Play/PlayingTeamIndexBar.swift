99986d5ded5ae2b40f020472ad4fedb8a4bf1642
corykim0829 <corykim0829@gmail.com>
2020-05-13 16:58:14 +0900

    feat: PlayingTeamIndexBar 커스텀 클래스
    cornerRadius 설정을 위해 구현
    
    Issue: #22

--- client-mobile/BaseballGameApp/BaseballGameApp/Views/Play/PlayingTeamIndexBar.swift
+++ client-mobile/BaseballGameApp/BaseballGameApp/Views/Play/PlayingTeamIndexBar.swift
@@ -1,15 +1,18 @@
 //
-//  CountBoardView.swift
+//  PlayingTeamIndexBar.swift
 //  BaseballGameApp
 //
-//  Created by Cory Kim on 2020/05/11.
+//  Created by Cory Kim on 2020/05/13.
 //  Copyright © 2020 corykim0829. All rights reserved.
 //
 
 import UIKit
 
-class CountBoardView: UIView {
+@IBDesignable
+class PlayingTeamIndexBar: UIView {
 
+    private let cornerRadius: CGFloat = 2.0
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -21,6 +24,6 @@ class CountBoardView: UIView {
     }
     
     private func configure() {
-        
+        layer.cornerRadius = cornerRadius
     }
 }
