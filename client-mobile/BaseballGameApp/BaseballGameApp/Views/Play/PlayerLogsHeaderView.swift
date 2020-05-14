8b1f779aba9cbe9284f8832c80e2fe0cb0852ac1
corykim0829 <corykim0829@gmail.com>
2020-05-14 17:33:39 +0900

    feat: PlayerLogsTableView HeaderView 수정
    Label 크기, borderWidth 변경 및 폰트 변경
    
    Issue: #22

--- client-mobile/BaseballGameApp/BaseballGameApp/Views/Play/PlayerLogsHeaderView.swift
+++ client-mobile/BaseballGameApp/BaseballGameApp/Views/Play/PlayerLogsHeaderView.swift
@@ -13,8 +13,8 @@ class PlayerLogsHeaderView: UITableViewHeaderFooterView {
     static let reuseIdentifier: String = String(describing: self)
     static let height: CGFloat = 44
     
-    private let playerNumberLabel: PlainLabel = PlainLabel(text: "1", color: .white, fontSize: 14, weight: .medium, alignment: .center)
-    private let playerNameLabel: PlainLabel = PlainLabel(text: "류현진", color: .white, fontSize: 17, weight: .semibold, alignment: .center)
+    private let playerNumberLabel: PlainLabel = PlainLabel(text: "1번", color: .white, fontSize: 14, weight: .medium, alignment: .center)
+    private let playerNameLabel: PlainLabel = PlainLabel(text: "류현진", color: .white, fontSize: 16, weight: .semibold, alignment: .center)
     
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
@@ -34,7 +34,7 @@ class PlayerLogsHeaderView: UITableViewHeaderFooterView {
     private func configureUI() {
         contentView.backgroundColor = UIColor(named: "board.view")
         playerNumberLabel.backgroundColor = UIColor(named: "count.board.inner.bg")
-        playerNumberLabel.layer.borderWidth = 1
+        playerNumberLabel.layer.borderWidth = 0.5
         playerNumberLabel.layer.borderColor = UIColor.white.cgColor
         playerNumberLabel.layer.cornerRadius = 4
         addSubview(playerNumberLabel)
@@ -42,7 +42,7 @@ class PlayerLogsHeaderView: UITableViewHeaderFooterView {
     }
     
     private func configureLayout() {
-        playerNumberLabel.constraints(topAnchor: topAnchor, leadingAnchor: leadingAnchor, bottomAnchor: bottomAnchor, trailingAnchor: nil, padding: .init(top: 12, left: 24, bottom: -12, right: 0), size: .init(width: 32, height: 0))
+        playerNumberLabel.constraints(topAnchor: topAnchor, leadingAnchor: leadingAnchor, bottomAnchor: bottomAnchor, trailingAnchor: nil, padding: .init(top: 10, left: 24, bottom: -10, right: 0), size: .init(width: 40, height: 0))
         playerNameLabel.constraints(topAnchor: nil, leadingAnchor: playerNumberLabel.trailingAnchor, bottomAnchor: nil, trailingAnchor: nil, padding: .init(top: 0, left: 12, bottom: 0, right: 0))
         playerNameLabel.centerYAnchor.constraint(equalTo: playerNumberLabel.centerYAnchor).isActive = true
     }
