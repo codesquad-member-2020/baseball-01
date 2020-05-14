224300bbb43366d048b7b58ab7382e63a8d3db27
corykim0829 <corykim0829@gmail.com>
2020-05-14 22:21:52 +0900

    feat: MatchListUseCase 구현
    Issue: #23

--- client-mobile/BaseballGameApp/BaseballGameApp/UseCase/MatchUseCase.swift
+++ client-mobile/BaseballGameApp/BaseballGameApp/UseCase/MatchUseCase.swift
@@ -13,5 +13,33 @@ struct MatchListUseCase {
         var path: String = "http://13.124.60.97:8080/matches"
     }
     
-    
+    struct MatchListTask: NetworkTask {
+        typealias Output = Result<[Match]?, NetworkErrorCase>
+        
+        private let networkDispatcher: NetworkDispatcher
+        
+        init(networkDispatcher: NetworkDispatcher) {
+            self.networkDispatcher = networkDispatcher
+        }
+        
+        func perform(_ request: Request, completion: @escaping (Result<[Match]?, NetworkErrorCase>?) -> Void) {
+            networkDispatcher.execute(request: request) { (result) in
+                switch result {
+                case .success(let data):
+                    let matchList = try? JSONDecoder().decode([Match].self, from: data)
+                    completion(.success(matchList))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        static func requestGameRoom(from matchListRequest: MatchListRequest,
+                                    with matchListTask: MatchListTask,
+                                    completion: @escaping (Result<[Match]?, NetworkErrorCase>?) -> Void) {
+            matchListTask.perform(matchListRequest) { (result) in
+                completion(result)
+            }
+        }
+    }
 }
