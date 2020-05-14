3b0f6df8f8550f03de5f877b53355dbeb4c73d9f
corykim0829 <corykim0829@gmail.com>
2020-05-15 01:55:14 +0900

    hotfix: 서버 주소 변경

--- client-mobile/BaseballGameApp/BaseballGameApp/UseCase/MatchUseCase.swift
+++ client-mobile/BaseballGameApp/BaseballGameApp/UseCase/MatchUseCase.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct MatchListUseCase {
     struct MatchListRequest: Request {
-        var path: String = "http://13.124.60.97:8080/matches"
+        var path: String = "http://52.79.100.252:8080/matches"
     }
     
     struct MatchListTask: NetworkTask {
